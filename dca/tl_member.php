12101d89360f87b9805cf23021c1e41ed73b971d
Dennis Patzer <d.patzer@heimrich-hannot.de>
2018-08-03 17:10:28 +0200

    fixed backend layout issue

--- dca/tl_member.php
+++ dca/tl_member.php
@@ -11,17 +11,18 @@ $arrDca['palettes']['default'] = str_replace('{address_legend:hide}', '{address_
  * Fields
  */
 $arrDca['fields']['memberCompanies'] = [
-	'label'         => &$GLOBALS['TL_LANG']['tl_member']['memberCompanies'],
-	'filter'        => true,
-	'exclude'       => true,
-	'inputType'     => 'select',
-	'foreignKey'    => 'tl_company.title',
-	'relation'      => ['type' =>'belongsToMany', 'load' =>'eager'],
-	'eval'          => ['tl_class' => 'w100', 'chosen' => true, 'includeBlankOption' => true, 'multiple' => true, 'style' => 'width: 100%'],
-	'sql'           => "blob NULL"
+    'label'      => &$GLOBALS['TL_LANG']['tl_member']['memberCompanies'],
+    'filter'     => true,
+    'exclude'    => true,
+    'inputType'  => 'select',
+    'foreignKey' => 'tl_company.title',
+    'relation'   => ['type' => 'belongsToMany', 'load' => 'eager'],
+    'eval'       => ['tl_class' => 'clr w100', 'chosen' => true, 'includeBlankOption' => true, 'multiple' => true, 'style' => 'width: 100%'],
+    'sql'        => "blob NULL"
 ];
 
 
-class tl_member_companies extends \Backend {
+class tl_member_companies extends \Backend
+{
 
 }
--- dca/tl_user.php
+++ dca/tl_user.php
@@ -5,24 +5,26 @@ $arrDca = &$GLOBALS['TL_DCA']['tl_user'];
 /**
  * Palettes
  */
-foreach (array_keys($arrDca['palettes']) as $strPalette)
-	$arrDca['palettes'][$strPalette] = str_replace('email', 'email,userCompanies', $arrDca['palettes'][$strPalette]);
+foreach (array_keys($arrDca['palettes']) as $strPalette) {
+    $arrDca['palettes'][$strPalette] = str_replace('email', 'email,userCompanies', $arrDca['palettes'][$strPalette]);
+}
 
 /**
  * Fields
  */
 $arrDca['fields']['userCompanies'] = [
-	'label'         => &$GLOBALS['TL_LANG']['tl_user']['userCompanies'],
-	'filter'        => true,
-	'exclude'       => true,
-	'inputType'     => 'select',
-	'foreignKey'    => 'tl_company.title',
-	'relation'      => ['type' =>'belongsToMany', 'load' =>'eager'],
-	'eval'          => ['tl_class' => 'w100', 'chosen' => true, 'includeBlankOption' => true, 'multiple' => true, 'style' => 'width: 100%'],
-	'sql'           => "blob NULL"
+    'label'      => &$GLOBALS['TL_LANG']['tl_user']['userCompanies'],
+    'filter'     => true,
+    'exclude'    => true,
+    'inputType'  => 'select',
+    'foreignKey' => 'tl_company.title',
+    'relation'   => ['type' => 'belongsToMany', 'load' => 'eager'],
+    'eval'       => ['tl_class' => 'clr w100', 'chosen' => true, 'includeBlankOption' => true, 'multiple' => true, 'style' => 'width: 100%'],
+    'sql'        => "blob NULL"
 ];
 
 
-class tl_user_companies extends \Backend {
+class tl_user_companies extends \Backend
+{
 
 }
